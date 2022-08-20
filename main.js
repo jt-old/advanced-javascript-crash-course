0e81da1e0e4fc15f2fd4119c8e1ba97452c55da3
justintsugranes <justintsugranes@proton.me>
2022-08-20 11:26:59 -0600

    notes on classes, iterables

--- main.js
+++ main.js
@@ -126,10 +126,10 @@ It makes functions reusable by letting you decide the object value.
 This value is determined entirely by how a function is called. */
 
 /* function sayMyName(name) {
-  console.log('My name is ${name}')
+  console.log(`My name is ${name}`)
 }
 
-sayMyName('Walter White') */
+sayMyName('King Kong') */
 
 /*  You can determine the value of 'this' keyword by how a function is called.
 
@@ -143,21 +143,256 @@ There are four ways to invoke a function in JavaScirpt and determine the value o
 
 // 1. Implicit Binding
 
-const person = {
-  name: 'Justin',
+/* const person = {
+  name: 'Charlie Chaplin',
   sayMyName: function () {
-    console.log('My name is ${this.name}')
+    console.log(`My name is ${this.name}`)
   },
 }
 
-person.sayMyName() // My name is Justin
+person.sayMyName() // My name is Charlie Chaplin */
 
 /* The implicit binding rule states that when a funciton is invoked with the 'dot' notation, the object to the left of the dot is what this keyword is referencing. In the above example, JavaScript treats 'this.name' as 'person.name', which is equal to the string 'Justin' and the output  */
 
 // 2. Explicit Binding
 
-/*  */
+/* In JS, every function has a built in method called 'call' which allows you to specify the context with which a function is invoked.*/
+
+/* const person = {
+  name: 'Bill Clinton',
+  sayMyName: function () {
+    console.log(`My name is ${this.name}`)
+  },
+}
 
 function sayMyName() {
-  console.log('My name is ${this.name}')
+  console.log(`My name is ${this.name}`)
 }
+
+sayMyName.call(person) */
+
+// We have explicitly specified the context when the function is called.
+
+/* The first argument passed to call is what the 'this' keyword inside sayMyName is referencing. */
+
+// 3. 'new' Binding
+
+/* In this scenario, the function is invoked with the 'this' keywrod referncing an empty object. We can now create multiple people, passing in different names each time */
+
+/*  JavaScript under the hood will create a new empty object that the 'this' keyword will reference.*/
+
+/* function Person(name) {
+  // this is = {}
+  this.name = name
+}
+
+// Constructor function
+const p1 = new Person('Justin')
+const p2 = new Person('Batman')
+
+console.log(p1.name, p2.name) */
+
+// 4. Default Binding
+
+//This is the fallback binding if none of the other three rules are met
+/* function sayMyName() {
+  console.log(`My name is ${this.name}`)
+}
+
+function Person(name) {
+  // this is = {}
+  this.name = name
+}
+
+// Constructor function
+const p1 = new Person('Justin')
+const p2 = new Person('Batman')
+
+sayMyName() // My name is undefined. */
+
+/* None of the three rules are sitisfied, so JavaScript will default to the global scope and set the 'this' keyword to the global object. In the global scope, JS will try to find a variable called 'name'. Since it doesn't find it, this.name is undefined. */
+
+/* Order of Precedence For Binding
+
+1 - New Binding
+2 - Explicit Binding
+3 - Implicit Binding
+4 - Default Binding */
+
+// Prototype
+
+/* In JavaScript, every function has a property called 'prototype' that points to an object. We can make use of that protoyype object to determine all of our shareale properties.*/
+
+/* function Person(fName, lName) {
+  this.firstName = fName
+  this.lastName = lName
+}
+
+// Constructor function
+const person1 = new Person('Bruce ', 'Wayne')
+const person2 = new Person('Clark ', 'Kent')
+// Instead of : person1.getFullName = function (), to create a prototype, we use:
+Person.prototype.getFullName = function () {
+  return this.firstName + '' + this.lastName
+}
+
+console.log(person1.getFullName())
+console.log(person2.getFullName()) */
+
+// Prototypal Inheritance
+
+/* One use of 'prototype' is the share properties and methods across instances. Another use is prototypal inheritance. */
+
+/* function Person(fName, lName) {
+  this.firstName = fName
+  this.lastName = lName
+}
+
+Person.prototype.getFullName = function () {
+  return this.firstName + ' ' + this.lastName
+}
+
+function SuperHero(fName, lName) {
+  //this = {}
+  Person.call(this, fName, lName)
+  this.isSuperHero = true
+}
+SuperHero.prototype.fightCrime = function () {
+  console.log('Fighting crime')
+}
+SuperHero.prototype = Object.create(Person.prototype)
+
+const batman = new SuperHero('Bruce ', 'Wayne')
+console.log(batman.getFullName()) // Bruce Wayne
+SuperHero.prototype.constructor = SuperHero */
+
+/* Initially, Batman only has acces to  'isSuperHero' and 'fightcrime'. To inherit 'firstName' and 'lastName', modify the SuperHero function to accept the paramters, 'fName' and 'lName'. Then, add a 'call method' that specifies where 'this' points to. So, 'this' in Person now refers to 'this' in SuperHero. fName and lName and get assigned the firstName and lastName properties, and are inherited by the SuperHero function.
+
+To inhereit the getFullName method, we used Object.create, which is a method which will delegate to another object on failed lookups. So, SuperHero.prototype is equal to Object.create and we pass in a Person.prototype. When you try to access batman.getFullName, JavaScript checks the prototype object of SuperHero. It doesn't find it. However, it sees that the prototype object has a chain to Person.prototype. Because of the Object.create method, it checks to see if Person.prototype has a getFullName method. It does and will execute that method. This is how the method is inherited through the prototype chain. Hence the name, prototypal inheritance.
+
+Now, we can modify the SuperHero instance of batman, input first and last name, and then console log batman.getFullName(). batman SuperHero has inherited getFullName from Person. The method will access this.firstName and this.lastName, which are inherited by the SuperHero function.
+
+The last thing you need to do is to ensure SuperHero.prototype.constructor is equal to SuperHero. Otherwise, JavaScript thinks that a 'SuperHero' is created from 'Person', which is incorrect. SuperHero inherits properties and methods From 'Person'
+ */
+
+// Class
+
+/* 
+To recreate the code from Prototypal inheritance, first the Person function becomes class Person. Next, the initialization of the name arguments gets moved into a constructor that accepts 'fName' and 'lName'.
+*/
+
+/* class Person {
+  constructor(fName, lName) {
+    this.firstName = fName
+    this.lastName = lName
+  }
+  //Lastly, change all the methods on the prototype object to be rewritten as methods within the class.
+  sayMyName() {
+    return this.firstName + ' ' + this.lastName
+  }
+}
+// You can now create different instances of the Person.
+const classP1 = new Person('Bruce', 'Wayne')
+console.log(classP1.sayMyName()) // Bruce Wayne
+
+// We can create an instance and access the properties and methods like before. Nothing changes. The 'class' keyword simply makes the syntax better.
+
+// To rewrite 'SuperHero', we use two keywords that care of the entire inheritance, 'extends' and 'super'.
+
+// We specify that class SuperHero extends Person and in the constructor, we accept fName and lName. We invoke the 'super' method that JavaScript provides. In the 'super' method, we pass fName and lName. This will call the Person class constructor. Once we call 'super', we set the SuperHero properties and methods in the class. We set this.isSuperHero to true. Then, we define the fight crime method.
+class SuperHero extends Person {
+  constructor(fName, lName) {
+    super(fName, lName)
+    this.isSuperHero = true
+  }
+  fightCrime() {
+    console.log('Fighting crime')
+  }
+}
+
+const batman = new SuperHero('Bruce', 'Wayne')
+console.log(batman.sayMyName()) */
+
+// Here is the entire code without notes:
+
+/* class Person {
+  constructor(fName, lName) {
+    this.firstName = fName
+    this.lastName = lName
+  }
+  sayMyName() {
+    return this.firstName + ' ' + this.lastName
+  }
+}
+
+const classP1 = new Person('Bruce', 'Wayne')
+console.log(classP1.sayMyName())
+
+class SuperHero extends Person {
+  constructor(fName, lName) {
+    super(fName, lName)
+    this.isSuperHero = true
+  }
+  fightCrime() {
+    console.log('Fighting crime')
+  }
+}
+
+const batman = new SuperHero('Bruce', 'Wayne')
+console.log(batman.sayMyName()) */
+
+// The important takeaways are: how to create a class, how to initialize properties, how to add methods, how to create an instance of the class, how to inherit using the 'extends' and 'super' keywords.
+
+// Iterables and Iterators
+
+/* Before 2013, JavaScript only had three looping contructs: 'for' 'while' and 'do while' */
+
+// For Loop
+// Here's how to iterate and access data with string and array types using the 'for' loop.
+
+/* // String
+const str = 'Bennigans'
+
+// for loop
+for (let i = 0; 1 < str.length; i++) {
+  console.log(str.charAt(i))
+}
+
+// Array
+const arr = ['B', 'e', 'n', 'n', 'i', 'g', 'a', 'n', 's']
+
+// for loop
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+} */
+
+/* These methods make it difficult to access the element and difficult to manage iteration on the dats for varius types of data structures.
+
+We need to create a new variable 'i', keep track of it to ensure it satisfies a condition and finally increment that vlaue of 'i' to access the next eleemtn in the collection. If there are nested 'for' loops, we have to do this all over again with a second variable. So, it would be difficult to access the element itself before even getting to the part where we do something with that element.
+
+Pertaining to data structure, it is difficult to manage iteration on data for various types of data structures. Iterating and accessing elements on a string is different from iterating on an array.
+
+Iterables and Iterators were introduced in 2015 and offer a way to iterate over various data structures in a way that abstracts away the complexity of accessing elements one by one and are, at the same time, uniform across the different data structures. This makes code more readable and less confusing. Iterables and Iterators make it possible to access data from a collection one at a time, which allows us to focus on what to do with the data, rather than how to access the data. 
+
+Some data structures, including Strings, Arrays, Maps and Sets now implement the iterable protocol by default. They are termed as Built In Iterables.
+
+A new looping construct was also introduced in 2015 to iterate over an iterable: the 'for of' loop.
+*/
+
+// For..of Loop
+
+//String
+const str = 'Bennigans'
+
+for (const char of str) {
+  console.log(char)
+}
+
+// Array
+const arr = ['B', 'e', 'n', 'n', 'i', 'g', 'a', 'n', 's']
+
+for (const item of arr) {
+  console.log(item)
+}
+
+// We don't really have to worry about accessing the element from the data structure. It is just given to us one by one in a sequence, allowing us to focus on the functionality. In this example, we are just logging to the console, but you can add any functionality you want.
